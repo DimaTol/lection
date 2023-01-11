753e2b14e6508be471cfa67471d5e105c4330e70
DimaTol <d.tolkachenko@ya.ru>
2023-01-11 13:52:40 +0300

    файл

--- file.py
+++ file.py
@@ -16,22 +16,3 @@ data.close()
 
 
 
-
-
-dictionary = {}
-dictionary = \
-{
-    'up': '↑',
-    'left': '←',
-    'down': '↓',
-    'right': '→'
-}
-
-print (dictionary)
-print(dictionary['left'])
-print(dictionary['up'])
-print(dictionary['down'])
-print(dictionary['right'])
-
-for item in dictionary:
-    print('{}: {}'.format(item, dictionary[item]))
