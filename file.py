446ac6aa90f4451304094c4e7cc4e5c3f88741e3
DimaTol <d.tolkachenko@ya.ru>
2023-01-11 14:15:02 +0300

    файл

--- file.py
+++ file.py
@@ -1,18 +1,30 @@
 
+
+
+
 with open('file.txt', 'w') as data:
     data.write('line 1\n')
     data.write('line 2\n')
+    data.write('Вот еще третья строчка 3\n')
 
-colors = ['red', 'green', 'blue']
+
+colors = ['а теперь добавляем это']
 data = open('file.txt', 'a')
 data.writelines(colors) # разделителей не будет
 data.close()
+
 
 path = 'file.txt'
 data = open(path, 'r')
 for line in data:
     print(line)
 data.close()
+
+
+# a – открытие для добавления данных
+# r – открытие для чтения данных
+# w – открытие для записи данных
+# w+, r+
 
 
 
